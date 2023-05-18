b3eebb5d6d8336cfdcafc2ebf8cfefb0703b96d9
LuluOrosB <maria@bisonic.io>
2023-05-18 11:05:58 -0600

    ADD available extractions on g node

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ df1 = read_wild_depositchances(event['SAMPLE_SPREADSHEET_ID'], event['SAMPLE_RAN
 
 #CONTROLS
 st.sidebar.markdown("## Controls")
-multiplier = st.sidebar.slider('Available Extractions on G Node', min_value=1, max_value=50, value=13, step=1)
+multiplier = st.sidebar.slider('Available Extractions on G Node', min_value=1, max_value=100, value=30, step=5)
 #epm = st.sidebar.slider('Extractions per minute', min_value=1, max_value=50, value=5, step=1)
-gems_nu = st.sidebar.slider('Gems G Nodes used', min_value=1, max_value=50, value=2, step=1)
-els_nu = st.sidebar.slider('Elements G Nodes used', min_value=1, max_value=50, value=2, step=1)
+gems_nu = st.sidebar.slider('Gems G Nodes used', min_value=1, max_value=50, value=4, step=1)
+els_nu = st.sidebar.slider('Elements G Nodes used', min_value=1, max_value=50, value=3, step=1)
 met_nu = st.sidebar.slider('Metals G Nodes used', min_value=1, max_value=50, value=2, step=1)
 fab_nu = st.sidebar.slider('Fabrics G Nodes used', min_value=1, max_value=50, value=2, step=1)
 woods_nu = st.sidebar.slider('Woods G Nodes used', min_value=1, max_value=50, value=2, step=1)
