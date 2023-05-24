e9f6271407ab84f71b1ed9bda4dc3e5856817bd8
mariaob1201 <maria.oros1201@gmail.com>
2023-05-24 15:32:32 -0600

    physic elements count

--- source/recipes.py
+++ source/recipes.py
@@ -294,7 +294,7 @@ def items_summary(df, tier, els, title, ememies_items, gnodes_items):
                 count_gold['GOLD'] = df1[el].sum()
             else:
                 if df1[el].sum() > 0:
-                    count_fis[el] = int(df1[el].sum() / element_multiplier(el))
+                    count_fis[el] = int(df1[el].sum())
                 else:
                     pass
 
@@ -307,7 +307,7 @@ def items_summary(df, tier, els, title, ememies_items, gnodes_items):
         fisi_df = pd.DataFrame.from_dict(
             {'Items': list(count_fis.keys()),
              'Family': [d_type(i) for i in list(count_fis.keys())],
-             'ExtractionsRequiredOnRecipe': list(count_fis.values())
+             'ExtractionsRequiredOnRecipe': [count_fis[el] for el in count_fis.keys()/element_multiplier(el)]
              })
 
         # elite_b = -count_gold['GOLD']+enemies_gold['Elite']
